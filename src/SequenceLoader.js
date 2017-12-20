8697ced9662fc44980b08babfa85eb80c2752391
tkellehe <mitchell.kelleher@gmail.com>
2017-12-20 15:34:37 -0500

    Update SequenceLoader.js

--- src/SequenceLoader.js
+++ src/SequenceLoader.js
@@ -5,19 +5,39 @@ var SequenceLoader = {
   _sequences: {}
 };
 
+var parent = document.getElementsByTagName('script');
+parent = parent[parent.length-1].parentNode;
+  
 // Loads a NumberSequence.
 SequenceLoader.load = function(name) {
-  return false;
+  // get some kind of XMLHttpRequest
+  var xhrObj = createXMLHTTPObject();
+  // open and send a synchronous request
+  xhrObj.open('GET', "https://tkellehe.github.io/hazel/src/sequences/" + name, false);
+  xhrObj.send('');
+  
+  if(!xhrObj.responseText) return false;
+  
+  // add the returned content to a newly created script tag
+  var se = document.createElement('script');
+  se.type = "text/javascript";
+  se.text = xhrObj.responseText;
+  parent.appendChild(se);
+  return true;
 }
 
 // Checks to see a NumberSequence can even be loaded.
 SequenceLoader.has = function(name) {
-  return false;
+  return SequenceLoader._squences[name] !== undefined;
 }
 
 // Finds the NumberSequence specified and will create a default one if cannot.
 SequenceLoader.get = function(name) {
-  return new NumberSequence();
+  if(SequenceLoader.has(name)) {
+    return SequenceLoader._squences[name];
+  } else {
+    return new NumberSequence();
+  }
 }
 
 // Publish the sequence loader interface.
